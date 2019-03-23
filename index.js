9dcf2c7c47b3853efdaeb1c4a08b957ed15d208e
ZoesDev <6062364+ZoesDev@users.noreply.github.com>
2019-03-23 11:05:42 -0700

    removed the need for monogdb for now

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const expressValidator = require('express-validator');
 const expressStatusMonitor = require('express-status-monitor');
 const sass = require('node-sass-middleware');
 const multer = require('multer');
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 const db = require('./libs/database.js');
 
